9dadbfaa66acb3112ee8a6305935e6f8c875d456
Jonny Goff-White <jhg14@ic.ac.uk>
2016-05-20 16:53:13 +0100

    Added arrows to game view - to make opaque in response to accelerometer

---
+++ bluetooth/InGameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InGameViewController.swift
+//  bluetooth
+//
+//  Created by Jonny Goff-White on 20/05/2016.
+//  Copyright © 2016 webapps. All rights reserved.
+//
+
+import UIKit
+
+class InGameViewController: UIViewController {
+    
+    
+    @IBOutlet weak var northArrow: UIImageView!
+    @IBOutlet weak var eastArrow: UIImageView!
+    @IBOutlet weak var southArrow: UIImageView!
+    @IBOutlet weak var westArrow: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func fadeNorth () {
+    }
+    
+}
