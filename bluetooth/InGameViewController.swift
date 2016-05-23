b78a33b6ee9f3727ba9f5b8e4642113341c45497
Jonny Goff-White <jhg14@ic.ac.uk>
2016-05-23 16:43:55 +0100

    Added variation of opacity of arrows in accordance with accelerometer data - needs refining

--- bluetooth/InGameViewController.swift
+++ bluetooth/InGameViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreMotion
 
 class InGameViewController: UIViewController {
     
+    var motionManager: CMMotionManager!
     
     @IBOutlet weak var northArrow: UIImageView!
     @IBOutlet weak var eastArrow: UIImageView!
@@ -18,9 +20,39 @@ class InGameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    func fadeNorth () {
+        
+        northArrow.alpha = 0
+        eastArrow.alpha  = 0
+        southArrow.alpha = 0
+        westArrow.alpha  = 0
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        
+        motionManager.accelerometerUpdateInterval = (1/50)
+        
+        if (motionManager.accelerometerAvailable) {
+            let queue = NSOperationQueue()
+            motionManager.startAccelerometerUpdatesToQueue(queue, withHandler: { (accelerometerData: CMAccelerometerData?, error: NSError?) in
+                dispatch_async(dispatch_get_main_queue(), { 
+                    guard let data = accelerometerData else {
+                        return
+                    }
+                    
+                    if (data.acceleration.x >= 0) {
+                        self.eastArrow.alpha = CGFloat(data.acceleration.x)
+                    } else {
+                        self.westArrow.alpha = CGFloat(-data.acceleration.x)
+                    }
+                    
+                    if (data.acceleration.y >= 0) {
+                        self.northArrow.alpha = CGFloat(data.acceleration.y)
+                    } else {
+                        self.southArrow.alpha = CGFloat(-data.acceleration.y)
+                    }
+                })
+            })
+        }
     }
     
 }
