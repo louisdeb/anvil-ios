7aad4424a4657300e36fded7f4be096f0939432d
Jonny Goff-White <jhg14@ic.ac.uk>
2016-06-03 12:44:45 +0100

    Cleaned up button code

--- bluetooth/ControllerViewController.swift
+++ bluetooth/ControllerViewController.swift
@@ -20,51 +20,8 @@ class ControllerViewController: UIViewController {
         controls?.forEach({ (elem) in 
             
             elem.userInteractionEnabled = true
-            
-            let tap = UITapGestureRecognizer(target: self, action: #selector(ControllerViewController.depressButton(_:)))
-            tap.delegate = self
-            self.view.addGestureRecognizer(tap)
-            self.view.addSubview(elem)
         })
         
         print(filenameToView)
     }
-}
-
-
-//Depress Button
-extension ControllerViewController: UIGestureRecognizerDelegate {
-    
-    func depressButton(sender: UITapGestureRecognizer) {
-        
-        if sender.state == .Began {
-        
-            controls?.forEach({ (elem) in
-                if CGRectContainsPoint(elem.frame, sender.locationInView(self.view)) {
-                    changeButtonToPressed(elem as! UIImageView)
-                }
-            })
-        } else {
-            print("Derkhead")
-        }
-    }
-}
-
-//Utility
-extension ControllerViewController {
-    
-    func changeButtonToPressed(button: UIImageView) {
-        let filename = filenameToView![button]
-        print("The filename is: \(filename)\n")
-        let newFilename = filename! + "_pressed"
-        let newImage = UIImage(named: newFilename)
-        button.image = newImage
-    }
-    
-    func changePressedToButton(button: UIImageView) {
-        let filename = filenameToView![button]
-        let newImage = UIImage(named: filename!)
-        button.image = newImage
-    }
-    
 }
