5018a222cee768b6991f5ac4cbe83bf03bf1c594
Jono Muller <jonomuller@gmail.com>
2016-06-08 17:29:11 +0100

    Skeleton login screen files and view controller added - LoginViewController class created - Login button added to initial view controller which goes to Login view controller

---
+++ bluetooth/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  bluetooth
+//
+//  Created by Jono Muller on 08/06/2016.
+//  Copyright © 2016 webapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
