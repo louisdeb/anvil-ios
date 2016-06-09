df788ad073d0bd34aefcb49590a99137360b5244
Louis de Beaumont <debeaumont.louis@googlemail.com>
2016-06-09 19:39:47 +0100

    Controller view now accessible by button from config view

--- bluetooth/ConfigurationSelectViewController.m
+++ bluetooth/ConfigurationSelectViewController.m
@@ -3,9 +3,21 @@
 @implementation ConfigurationSelectViewController
 
 int const PAN_TRANSLATION_MIN = 200;
+NSString *const CONTROLLER_SEGUE = @"controllerSegue";
 
 -(void)viewDidLoad {
   [super viewDidLoad];
+}
+
+-(IBAction)selectView:(id)sender {
+  [self performSegueWithIdentifier:CONTROLLER_SEGUE sender:sender];
+}
+
+-(IBAction)handlePan:(UIPanGestureRecognizer*) sender {
+  CGPoint translation = [sender translationInView: self.view];
+  
+  if (fabs(translation.y) > PAN_TRANSLATION_MIN)
+    [self dismissViewControllerAnimated:true completion:nil];
 }
 
 
