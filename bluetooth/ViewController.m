89c0eb57684b482f0d3ac8cea9df81cade9c1811
Louis de Beaumont <debeaumont.louis@googlemail.com>
2016-06-13 10:48:05 +0100

    Adjusted position and speed of bluetooth gif

--- bluetooth/ViewController.m
+++ bluetooth/ViewController.m
@@ -63,8 +63,8 @@ NSString *const KEYCHAIN_SERVICE = @"Anvil";
   CGFloat height = 200;
   CGFloat width = 200;
   CGFloat posX = (self.view.frame.size.width / 2) - (width / 2);
-  CGFloat posY = (self.view.frame.size.height / 2) - (height / 2);
-  CGFloat speed = 3.2;
+  CGFloat posY = (self.view.frame.size.height / 2) - (height / 2) - 70;
+  CGFloat speed = 3;
   
   UIImageView* animatedImageView = [[UIImageView alloc] initWithFrame:CGRectMake(posX, posY, width, height)];
   NSMutableArray *tempArray = [[NSMutableArray alloc] initWithCapacity:90];
